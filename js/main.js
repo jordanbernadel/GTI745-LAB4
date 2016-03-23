74ebd30277d7d25b984c776de2a504c10a004202
Jordan Bernadel <bernadel.jordan@gmail.com>
2016-03-23 13:56:03 -0400

    update HD af

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.main = {
     init : function() {
         $(".btn-file :file").bind("change", this.fileOnChange);
-        $(".fa-bar-chart").bind("click", this.showBarChart)
+        $(".fa-bar-chart").bind("click", this.showBarChart);
     },
 
     fileOnChange : function(){
@@ -15,15 +15,14 @@ window.main = {
         $(".tools").removeClass("hide").css("opacity", 1);
 
         d3.csv(label, function(data) {
-            dataset = data.map(function(d) { return [   +d["ID"], 
-                                                        d["Prenom"],
-                                                        d["Nom"],
+            dataset = data.map(function(d) { return [   +d["id"], 
                                                         d["Sexe"],
                                                         +d["Age"],
-                                                        d["Pays"],
-                                                        +d["VitesseMoyenne"],
-                                                        d["CouleurVoiture"],
-                                                        d["MarqueVoiture"]
+                                                        d["Continent"],
+                                                        +d["NbCafeSemaine"],
+                                                        +d["NbBiereSemaine"],
+                                                        d["TransportFavori"],
+                                                        d["PaiementFavori"]
                                                     ];});
 
             // csv modal content
@@ -34,71 +33,129 @@ window.main = {
                 .append("div")
                 .attr("class", "row")
                 .html(function(d){ 
-                    var id              = d[0],
-                        prenom          = d[1],
-                        nom             = d[2],
-                        sexe            = d[3],
-                        age             = d[4],
-                        pays            = d[5],
-                        vitesse_moyenne = d[6],
-                        couleur_voiture = d[7],
-                        marque_voiture  = d[8];
+                    var id                  = d[0],
+                        sexe                = d[1],
+                        age                 = d[2],
+                        continent           = d[3],
+                        nb_cafe_semaine     = d[4],
+                        nb_biere_semaine    = d[5],
+                        transport_favori    = d[6],
+                        paiement_favori     = d[7];
 
                     var line =  "<div class='col-md-1 center'>"+id+"</div> " +
-                                "<div class='col-md-2 center'>"+prenom+"</div> " +
-                                "<div class='col-md-2 center'>"+nom+"</div> " +
                                 "<div class='col-md-1 center'>"+sexe+"</div> " +
                                 "<div class='col-md-1 center'>"+age+"</div> " +
-                                "<div class='col-md-1 center'>"+pays+"</div> " +
-                                "<div class='col-md-1 center'>"+vitesse_moyenne+"</div>"+
-                                "<div class='col-md-2 center'>"+marque_voiture+"</div>"+
-                                "<div class='col-md-1 center car_color' style='background-color:"+couleur_voiture+";'>"+couleur_voiture+"</div>";
+                                "<div class='col-md-2 center'>"+continent+"</div> " +
+                                "<div class='col-md-1 center'>"+nb_cafe_semaine+"</div> " +
+                                "<div class='col-md-2 center'>"+nb_biere_semaine+"</div> " +
+                                "<div class='col-md-2 center'>"+transport_favori+"</div>"+
+                                "<div class='col-md-2 center'>"+paiement_favori+"</div>";
                     return line; 
                 });
             $("#file_container.btn-success").bind("click", main.showData);
 
             // Bar chart
 
-            var chart = d3.select("#bar_chart");
+            var chart_var_1 = d3.select("#bar_chart_var_1");
+            var chart_var_2 = d3.select("#bar_chart_var_2");
 
             var barWidth = 50;
 
-            var car_brands = d3.map(dataset, function(d){return d[8];}).keys();
+            var continent_array = d3.map(dataset, function(d){return d[3];}).keys();
 
-            var car_brands_count = Array.apply(null, Array(car_brands.length)).map(Number.prototype.valueOf,0);;
+            var continent_male_count = Array.apply(null, Array(continent_array.length)).map(Number.prototype.valueOf,0);
+
+            var continent_female_count = Array.apply(null, Array(continent_array.length)).map(Number.prototype.valueOf,0);
 
             for(var i = 0; i < dataset.length; i++){
-                for(var j = 0; j < car_brands.length; j++){
-                    if(dataset[i][8] == car_brands[j]){
-                        car_brands_count[j] += 1;
+                for(var j = 0; j < continent_array.length; j++){
+                    if(dataset[i][3] == continent_array[j]){
+                        if(dataset[i][1] == "M"){
+                            continent_male_count[j] += 1;
+                        }else if(dataset[i][1] == "F"){
+                            continent_female_count[j] += 1;
+                        }
                     }
                 }
             }
 
-            var bar = chart.selectAll("div")
-                .data(car_brands)
+            var bar_male = chart_var_1.selectAll("div")
+                .data(continent_array)
                 .enter()
                 .append("div")
-                .attr("class", "bar")
-                .attr("style",function(d, i){ return "height:"+car_brands_count[i]*4+"px";})
+                .attr("class", "bar male init")
+                .on("mouseenter", main.showBarChartInfo)
+                .on("mouseleave", main.hideBarChartInfo)
+                .attr("style",function(d, i){ return "height:"+continent_male_count[i]*2+"px";})
+                .append("div")
+                .html(function(d, i){ return continent_male_count[i];});
+
+            var bar_female = chart_var_2.selectAll("div")
+                .data(continent_array)
+                .enter()
+                .append("div")
+                .attr("class", "bar female init")
+                .on("mouseenter", main.showBarChartInfo)
+                .on("mouseleave", main.hideBarChartInfo)
+                .attr("style",function(d, i){ return "height:"+continent_female_count[i]*2+"px";})
+                .append("div")
+                .html(function(d, i){ return continent_female_count[i];});
+
+
+            var left_male_counter = 450;
+            var gap = 150;
+            $( "#bar_chart div.male" ).each(function() {                
+                $(this).css("left", left_male_counter);
+                left_male_counter += gap;
+            });
+
+            left_male_counter = 450;
+            var left_female_counter = left_male_counter + barWidth + 1;
+            $( "#bar_chart div.female" ).each(function() {                
+                $(this).css("left", left_female_counter);
+                left_female_counter += gap;
+            });
+
+            var bar_x_axis = d3.select("#bar_chart_x_axis");
+
+            var x_axis_domain = bar_x_axis.selectAll("div")
+                .data(continent_array)
+                .enter()
                 .append("div")
                 .html(function(d){ return d;});
 
-            var left_counter = 390;
-            $( "#bar_chart div" ).each(function() {                
-                $(this).css("left", left_counter);
-                left_counter += 26;
-            });
+            
         });
     },
 
     showBarChart : function(){
-        $("#bar_chart_content").css("opacity", 1);
+        if($("#bar_chart_content").css("opacity") == 0){
+            $("#bar_chart_content").css("opacity", 1);
+            $(".fa-bar-chart").addClass("active");
+            $("#bar_chart .bar").removeClass('init');
+        }else{
+            $("#bar_chart_content").css("opacity", 0);
+            $(".fa-bar-chart").removeClass("active");
+            $("#bar_chart .bar").addClass('init');
+        }
+        
     },
 
     showData : function(){
         $('#csv_content').modal('show');
-    }
+    },
+
+    showBarChartInfo : function(){
+        $(this).children().css("opacity", 1);
+        $(this).children().css("font-size", 60);
+        $(this).children().css("margin-top", -150);
+    },
+
+    hideBarChartInfo : function(){
+        $(this).children().css("opacity", 0);
+        $(this).children().css("font-size", 10);
+        $(this).children().css("margin-top", 10);
+    },
 };
 
 $(document).ready(function() {
