bd413194c12af95ab41198f8f1b5f2f1484d57ce
Alex Kua <alexkua@me.com>
2016-03-27 02:39:34 -0400

    Fix bugs and coord parallele init

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ window.main = {
         $(".fa-bar-chart").bind("click", this.showBarChart);
         $(".fa-tree").bind("click", this.showTreeDiagram);
         $(".fa-cube").bind("click", this.show3dDiagram);
+        $(".fa-bars").bind("click", this.showParallelCordsDiagram);
     },
 
     fileOnChange : function(){
+        $("#chart_container").css("display","inherit");
         var input = $(this),
         label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
         $("#csv_file_name").val(label);
@@ -321,8 +323,6 @@ window.main = {
 
                 //Adding countries to select
 
-                alert(data.length);
-
                 countryData.forEach(function(d) {
                   countryById[d.id] = d.name;
                   continentById[d.id] = d.continent;
@@ -400,6 +400,132 @@ window.main = {
                 };
 
             };
+
+            /** parallel cords **/
+
+            var margin = {top: 30, right: 10, bottom: 10, left: 10},
+                width = 960 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
+
+            var x = d3.scale.ordinal().rangePoints([0, width], 1),
+                y = {},
+                dragging = {};
+
+            var line = d3.svg.line(),
+                axis = d3.svg.axis().orient("left"),
+                background,
+                foreground;
+
+            var svgpar = d3.select("#parallelcords_diagram").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            d3.csv("cars.csv", function(error, cars) {
+
+                // Extract the list of dimensions and create a scale for each.
+                x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
+                    return d != "name" && (y[d] = d3.scale.linear()
+                    .domain(d3.extent(cars, function(p) { return +p[d]; }))
+                    .range([height, 0]));
+                }));
+
+                // Add grey background lines for context.
+                background = svgpar.append("g")
+                    .attr("class", "background")
+                    .selectAll("path")
+                    .data(cars)
+                    .enter().append("path")
+                    .attr("d", path);
+
+                // Add blue foreground lines for focus.
+                foreground = svgpar.append("g")
+                    .attr("class", "foreground")
+                    .selectAll("path")
+                    .data(cars)
+                    .enter().append("path")
+                    .attr("d", path);
+
+                // Add a group element for each dimension.
+                var g = svgpar.selectAll(".dimension")
+                    .data(dimensions)
+                    .enter().append("g")
+                    .attr("class", "dimension")
+                    .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+                    .call(d3.behavior.drag()
+                    .origin(function(d) { return {x: x(d)}; })
+                    .on("dragstart", function(d) {
+                    dragging[d] = x(d);
+                    background.attr("visibility", "hidden");
+                })
+                .on("drag", function(d) {
+                    dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+                    foreground.attr("d", path);
+                    dimensions.sort(function(a, b) { return position(a) - position(b); });
+                    x.domain(dimensions);
+                    g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+                })
+                .on("dragend", function(d) {
+                    delete dragging[d];
+                    transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+                    transition(foreground).attr("d", path);
+                    background
+                    .attr("d", path)
+                    .transition()
+                    .delay(500)
+                    .duration(0)
+                    .attr("visibility", null);
+                }));
+
+                // Add an axis and title.
+                g.append("g")
+                    .attr("class", "axis")
+                    .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+                    .append("text")
+                    .style("text-anchor", "middle")
+                    .attr("y", -9)
+                    .text(function(d) { return d; });
+
+                // Add and store a brush for each axis.
+                g.append("g")
+                    .attr("class", "brush")
+                    .each(function(d) {
+                        d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
+                    })
+                    .selectAll("rect")
+                    .attr("x", -8)
+                    .attr("width", 16);
+            });
+
+            function position(d) {
+                var v = dragging[d];
+                return v == null ? x(d) : v;
+            }
+
+            function transition(g) {
+                return g.transition().duration(500);
+            }
+
+            // Returns the path for a given data point.
+            function path(d) {
+                return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+            }
+
+            function brushstart() {
+                d3.event.sourceEvent.stopPropagation();
+            }
+
+            // Handles a brush event, toggling the display of foreground lines.
+            function brush() {
+                var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
+                extents = actives.map(function(p) { return y[p].brush.extent(); });
+                foreground.style("display", function(d) {
+                    return actives.every(function(p, i) {
+                        return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+                    }) ? null : "none";
+                });
+            }
         });
     },
 
@@ -443,6 +569,20 @@ window.main = {
                 $("#threed_diagram_content").css("display","none");
             });
             $(".fa-cube").removeClass("active");
+        }
+        
+    },
+
+    showParallelCordsDiagram : function(){
+        if($("#parallelcords_diagram_content").css("opacity") == 0){            
+            $("#parallelcords_diagram_content").css("display","inherit");
+            $("#parallelcords_diagram_content").stop().animate({opacity: 1}, 500);
+            $(".fa-bars").addClass("active");
+        }else{
+            $("#parallelcords_diagram_content").stop().animate({opacity: 0}, 500, function(){
+                $("#parallelcords_diagram_content").css("display","none");
+            });
+            $(".fa-bars").removeClass("active");
         }
         
     },
